1b870863819c48556cb79c7e95faffdc7f9e0e3a
Mihajlo Racic <mihajlo.rac@gmail.com>
2018-07-06 01:24:10 +0200

    filteri, prva verzija genersiana

--- src/main/java/limes/soft/HelloController.java
+++ src/main/java/limes/soft/HelloController.java
@@ -2,37 +2,21 @@ package limes.soft;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 @RestController
 public class HelloController {
 
     @Autowired
-    BankaRepository bankaRepository;
+    DBRepository DBRepository;
 
-    @GetMapping(value = "/getBanks")
-    public List<Banka> selectTest(){
-        return bankaRepository.findAll();
+    @GetMapping(value = "/fa")
+    public String asd(@RequestParam String startDate, @RequestParam String endDate){
+        return DBRepository.findlAllPregledTroskova(startDate,endDate).toString();
     }
-    @GetMapping(value = "/banks")
-    public List<Banka> selectTestMock(){
-        List<Banka> banks = new ArrayList<>();
-        Random rand = new Random();
-        for(int i =0;i < 1000;i++){
-            banks.add(new Banka(Integer.toString(i),"Novi Sad Banka"+Integer.toString(i), "340-255123-12312"+Integer.toString(i),i%(rand.nextInt(50) + 1)));
-        }
-
-
-
-        return banks;
+    @GetMapping(value="/kupcidobavljaci")
+    public String pregledTroskova(){
+        return DBRepository.findlAllKupcidobavljaci().toString();
     }
-    @GetMapping(value = "/hello")
-    public String hello(){
-        return "hello";
-    }
-
 }
---
+++ src/main/java/limes/soft/PregledTroskova.java
@@ -0,0 +1,61 @@
+package limes.soft;
+
+public class PregledTroskova {
+    int RbrStavke;
+    int RbrNaloga;
+    String Konto;
+    String SifKomint;
+    float Duguje;
+    float Potrazuje;
+
+    public PregledTroskova() {
+    }
+
+    public int getRbrStavke() {
+        return RbrStavke;
+    }
+
+    public void setRbrStavke(int rbrStavke) {
+        RbrStavke = rbrStavke;
+    }
+
+    public int getRbrNaloga() {
+        return RbrNaloga;
+    }
+
+    public void setRbrNaloga(int rbrNaloga) {
+        RbrNaloga = rbrNaloga;
+    }
+
+    public String getKonto() {
+        return Konto;
+    }
+
+    public void setKonto(String konto) {
+        Konto = konto;
+    }
+
+    public String getSifKomint() {
+        return SifKomint;
+    }
+
+    public void setSifKomint(String sifKomint) {
+        SifKomint = sifKomint;
+    }
+
+    public float getDuguje() {
+        return Duguje;
+    }
+
+    public void setDuguje(float duguje) {
+        Duguje = duguje;
+    }
+
+    public float getPotrazuje() {
+        return Potrazuje;
+    }
+
+    public void setPotrazuje(float potrazuje) {
+        Potrazuje = potrazuje;
+    }
+}
